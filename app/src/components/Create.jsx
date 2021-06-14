9c61d202a2fa558945d916aaef38093e864623ed
Adrian <adrianalexander17@gmail.com>
2021-06-13 22:42:53 -0400

    almost finished "Create" component

--- app/src/components/Create.jsx
+++ app/src/components/Create.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import Button from './Button'
+// import { useRef } from "react";
+import Button from "./Button";
+// import PopupContainer from "./PopupContainer";
 
 export default function Create() {
 
-
-    return (
-        <div className="h-32 w-64 rounded bg-gray-700">
-            <div className="flex justify-center flex-col p-2 px-4">
-                <p className="font-thin text-white text-center">Group Name</p>
-                <input type="text" className="rounded font-thin mt-2 mb-4" placeholder="Group Name"/>
-                <div className=" flex justify-center">
-                    <Button
-                        label="Submit"
-                    />
-                </div>
-                
-            </div>
+  return (
+    <div className="h-32 w-64 rounded bg-gray-700">
+      <div className="flex justify-center flex-col p-2 px-4">
+        <p className="font-thin text-white text-center">Group Name</p>
+        <input
+          type="text"
+          className="rounded font-thin mt-2 mb-4"
+          placeholder="Group Name"
+        />
+        <div className=" flex justify-center">
+          <Button label="Submit" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
