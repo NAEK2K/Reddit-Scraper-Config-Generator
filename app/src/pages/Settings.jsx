2c45fd0a818ec8a9885fc69ffde18621d6c336c2
Adrian <adrianalexander17@gmail.com>
2021-06-13 23:13:24 -0400

    Finished settings

--- app/src/pages/Settings.jsx
+++ app/src/pages/Settings.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 
 export default function Settings() {
-
-
     return (
-        <div className="bg-green p-32">
-            <p className="text-white">
-                This is the settings page.
-            </p>
+        <div className="w-full">
+            <p className="text-white text-4xl font-thin pb-20 pt-6 pl-6">Settings</p>
+            <p className="pt-96 pl-6 font-thin text-white">Made with ❤️ by <a href="https://naek.ca/">NAEK</a> and <a href="https://adrian-alexander.ca/">Adrian</a>!</p>            
         </div>
     );
 }
