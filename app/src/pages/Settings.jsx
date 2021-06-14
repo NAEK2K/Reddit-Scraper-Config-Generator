1f9663a70309902649970e9409b45cbf0c95ca40
Adrian <adrianalexander17@gmail.com>
2021-06-13 22:44:22 -0400

    started settings page

---
+++ app/src/pages/Settings.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Settings() {
+
+
+    return (
+        <div className="bg-green p-32">
+            <p className="text-white">
+                This is the settings page.
+            </p>
+        </div>
+    );
+}
